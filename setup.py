1d7eb8bcce7946b812036e36076404136c93abe7
mikepk <mikepk@pluralsight.com>
2017-05-22 15:04:57 -0600

    Change package name to spavro

--- setup.py
+++ setup.py
@@ -16,48 +16,55 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 try:
-  from setuptools import setup
+  from setuptools import setup, find_packages
 except ImportError:
   from distutils.core import setup
 from sys import version_info
-from Cython.Build import cythonize
+from distutils.extension import Extension
+try:
+    from Cython.Build import cythonize
+    USE_CYTHON = True
+except ImportError:
+    USE_CYTHON = False
 
 install_requires = []
 if version_info[:2] <= (2, 5):
     install_requires.append('simplejson >= 2.0.9')
 
+ext = '.pyx' if USE_CYTHON else '.c'
+extensions = [Extension("spavro.fast_binary", ["src/spavro/fast_binary" + ext])]
+
+import sys
+if USE_CYTHON:
+    sys.stderr.write("CYTHONIZING...\n")
+    extensions = cythonize(extensions)
+
+print("Packages: ", find_packages())
+# raise Exception("WHAT THE FRIG")
 setup(
-  name = 'avro',
-  version = '@AVRO_VERSION@',
-  packages = ['avro',],
-  package_dir = {'avro': 'src/avro'},
-  scripts = ["./scripts/avro"],
+  name='spavro',
+  version='2.0',
+  packages=['spavro'],
+  package_dir={'': 'src'},
+  scripts=["./scripts/avro"],
 
   #include_package_data=True,
-  package_data={'avro': ['LICENSE', 'NOTICE']},
+  package_data={'spavro': ['LICENSE', 'NOTICE']},
 
   # Project uses simplejson, so ensure that it gets installed or upgraded
   # on the target machine
-  install_requires = install_requires,
+  install_requires=install_requires,
 
   # fast avro code
-  ext_modules=cythonize("./src/avro/fast_binary.pyx"),
+  ext_modules=extensions,
   # metadata for upload to PyPI
-  author = 'Apache Avro',
-  author_email = 'dev@avro.apache.org',
-  description = 'Avro is a serialization and RPC framework.',
-  license = 'Apache License 2.0',
-  keywords = 'avro serialization rpc',
-  url = 'http://avro.apache.org/',
-  extras_require = {
+  author='Michael Kowalchik',
+  author_email='mikepk@pluralsight.com',
+  description='Spavro is a (sp)eedier avro serialization and RPC framework.',
+  license='Apache License 2.0',
+  keywords='avro serialization rpc',
+  url='http://avro.apache.org/',
+  extras_require={
     'snappy': ['python-snappy'],
   },
 )
-
-
-# from distutils.core import setup
-# from Cython.Build import cythonize
-
-# setup(
-#     ext_modules=cythonize("./src/avro/fastbinary.pyx"),
-# )
