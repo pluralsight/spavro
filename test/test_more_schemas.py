a4e3935ce351a4823f5fb952851b9193fe44ab64
mikepk <mikepk@pluralsight.com>
2018-03-07 15:13:15 -0700

    Add test case for bytes in union

--- test/test_more_schemas.py
+++ test/test_more_schemas.py
@@ -5,7 +5,7 @@ import spavro.io
 from spavro.io import FastDatumWriter
 # from spavro.io import SlowDatumWriter as FastDatumWriter
 # from spavro.exceptions import AvroTypeException, SchemaParseException
-from spavro.schema import SchemaParseException, AvroException
+from spavro.schema import SchemaParseException, AvroException, UnionSchema
 
 
 from spavro import schema
@@ -21,22 +21,30 @@ invalid_schemas = (
          "size": 314}'''),
     ("enum_symbols_not_an_array", '''{"type": "enum",
          "name": "Status",
-         "symbols": "Normal Caution Critical"}''')
+         "symbols": "Normal Caution Critical"}'''),
+    ("missing_required_name_in_enum", '''{"type": "enum",
+         "symbols": ["Normal", "Caution", "Critical"]}''')
 )
+
+valid_schemas = (
+    ("bytes_in_a_union",
+        '["null", "string", "bytes"]',
+        UnionSchema),
+    ("bytes_and_fixed_in_a_union",
+        '["bytes", {"name": "sixteen-bytes", "type": "fixed", "size": 16}]',
+        UnionSchema),
+    )
 
 
 class TestSchemaParsing(unittest.TestCase):
     pass
 
 
-# def create_good_case(schema, datum):
-#     write_schema = spavro.schema.parse(schema)
-#     def test_write_good_data(self):
-#         fastbuff = BytesIO()
-#         fastencoder = spavro.io.FastBinaryEncoder(fastbuff)
-#         fdw = FastDatumWriter(write_schema)
-#         fdw.write(datum, fastencoder)
-#     return test_write_good_data
+def create_good_case(local_schema, expected_schema_type):
+    def test_write_good_data(self):
+        write_schema = spavro.schema.parse(local_schema)
+        self.assertIs(type(write_schema), expected_schema_type)
+    return test_write_good_data
 
 
 def create_exception_case(local_schema):
@@ -47,11 +55,11 @@ def create_exception_case(local_schema):
     return test_parse_invalid_schema
 
 
-# def make_good_cases(cases):
-#     for name, datum, schema in cases:
-#         test_method = create_good_case(schema, datum)
-#         test_method.__name__ = 'test_good_data_{}'.format(name)
-#         setattr(TestValidData, test_method.__name__, test_method)
+def make_good_cases(cases):
+    for name, local_schema, expected_schema_type in cases:
+        test_method = create_good_case(local_schema, expected_schema_type)
+        test_method.__name__ = 'test_good_schema_{}'.format(name)
+        setattr(TestSchemaParsing, test_method.__name__, test_method)
 
 
 def make_exception_cases(cases):
@@ -61,5 +69,5 @@ def make_exception_cases(cases):
         setattr(TestSchemaParsing, test_method.__name__, test_method)
 
 
-# make_good_cases(valid_data)
+make_good_cases(valid_schemas)
 make_exception_cases(invalid_schemas)
